206236fb6824ee3fdd6b9caa097c7bfbe75558c0
Michael <mdurfey@me.com>
2017-03-13 13:49:42 -0700

    mar-13-2017

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/index.js'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+// app.use('/static', express.static(path.join(__dirname, 'WeatherApp')))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
