dc7c3f938715b80f851a1565e13505b3c919ef0d
Michael <mdurfey@me.com>
2017-03-23 22:00:09 -0700

    fixed issue

--- app.js
+++ app.js
@@ -14,19 +14,17 @@ app.set('view engine', 'ejs');
 
 var request = require('request');
 
+
 app.get('/wundergroundInfo', function(req, res){
   var coords = req.query;
   var weatherURL = `https://api.wunderground.com/api/693a7e57a6eba40b/conditions/q/${coords.latitude},${coords.longitude}.json`;
-
-request(weatherURL, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-res.send(response.body);
-
-})
-
+  request(weatherURL, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(response.body);
+  })
 });
+
 
 app.listen(port, function () {
 
